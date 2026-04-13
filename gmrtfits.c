6ebb5ef383dceda890093fb053bcd38f87d0d147
shiningsurya <15906282+shiningsurya@users.noreply.github.com>
2026-04-13 11:24:43 +0530

    fold FITS writer

--- gmrtfits.c
+++ gmrtfits.c
@@ -7,17 +7,25 @@
 #define FLUSH_TIMES 8
 // flush once per FLUSH_TIMES rows
 
-void gmrtfits_prepare ( gmrtfits_t *fits, const char *filename, double mjd, unsigned npol, unsigned nchan, float fedge, float bw, unsigned nsblk, unsigned fftint, unsigned bitdepth ) {
+void gmrtfits_search_prepare ( gmrtfits_t *fits, const char *filename, double mjd, unsigned npol, unsigned nchan, float fedge, float bw, unsigned nsblk, unsigned fftint, unsigned bitdepth ) {
 	// sign(bw) determines LSB or USB
+	
+	fits->obsmode  = SEARCH;
 	
 	fits->filename = strdup ( filename );
 	
 	fits->status  = 0;
+	fits->nbin    = 1;
 	fits->npol    = npol;
 	fits->nchan   = nchan;
 	fits->nsblk   = nsblk;
 
-	sprintf ( fits->pol_type, "AABBCRCI" );
+	if ( fits->npol == 4 ) {
+		sprintf ( fits->pol_type, "AABBCRCI" );
+	} else if ( fits->npol == 1 ) {
+		sprintf ( fits->pol_type, "AA+BB" );
+	}
+
 
 	fits->nchan_npol       = nchan * npol;
 	fits->nsblk_nchan_npol = nsblk * nchan * npol;
@@ -26,6 +34,7 @@ void gmrtfits_prepare ( gmrtfits_t *fits, const char *filename, double mjd, unsi
 	fits->weights   = (float*)malloc ( sizeof(float) * nchan );
 	fits->scales    = (float*)malloc ( sizeof(float) * nchan * npol );
 	fits->offsets   = (float*)malloc ( sizeof(float) * nchan * npol );
+	fits->bata      = NULL;
 
 	fits->bitdepth  = bitdepth;
 	fits->fftint    = fftint;
@@ -41,33 +50,70 @@ void gmrtfits_prepare ( gmrtfits_t *fits, const char *filename, double mjd, unsi
 		fits->center_freqs [ ichan ] = fedge + ( ichan + 0.5 )*cbw;
 	}
 
-	fits->tsamp     = fftint * nchan / bw / 1E6;
-	fits->mjd       = mjd;
+	fits->tsamp_tbin = fftint * nchan / bw / 1E6;
+	fits->mjd        = mjd;
 }
 
-void gmrtfits_close ( gmrtfits_t *fits ) {
-	free ( fits->center_freqs );
-	free ( fits->offsets );
-	free ( fits->scales );
-	free ( fits->weights );
-	free ( fits->data );
-
-	free ( fits->filename );
+void gmrtfits_fold_prepare ( gmrtfits_t *fits, const char *filename, double mjd, unsigned npol, unsigned nchan, float fedge, float bw, unsigned nbin, double period ) {
+	// sign(bw) determines LSB or USB
 	
+	fits->period   = period;
+	fits->obsmode  = FOLD;
+	
+	fits->filename = strdup ( filename );
+	
+	fits->status  = 0;
+	fits->nbin    = nbin;
+	fits->npol    = npol;
+	fits->nchan   = nchan;
+	fits->nsblk   = 1;
 
-	fits_close_file ( fits->fits, &fits->status );
-	if ( fits->status ) {
-		fits_report_error(stderr, fits->status);
+	if ( fits->npol == 4 ) {
+		sprintf ( fits->pol_type, "AABBCRCI" );
+	} else if ( fits->npol == 1 ) {
+		sprintf ( fits->pol_type, "AA+BB" );
 	}
+
+
+	fits->nchan_npol       = nchan * npol;
+	fits->nbin_nchan_npol  = nbin * nchan * npol;
+
+	fits->bata      = (int16_t*) malloc ( sizeof(int16_t)  * nbin * nchan * npol );
+	/*fits->bata      = (float*) malloc ( sizeof(float)  * nbin * nchan * npol );*/
+	fits->weights   = (float*)malloc ( sizeof(float) * nchan );
+	fits->scales    = (float*)malloc ( sizeof(float) * nchan * npol );
+	fits->offsets   = (float*)malloc ( sizeof(float) * nchan * npol );
+	fits->data      = NULL;
+
+	/* default set to unity; data is saved as int16 */
+	fits->bitdepth  = 1;
+
+	if (bw < 0) fits->bandwidth_mhz = -1.0 * bw;
+	else fits->bandwidth_mhz  = bw;
+
+	int ichan            = 0;
+	fits->center_freqs   = (float*) malloc ( sizeof(float) * nchan );
+	float  cbw           = bw / nchan;
+	for (;ichan < nchan; ichan++) {
+		fits->center_freqs [ ichan ] = fedge + ( ichan + 0.5 )*cbw;
+		/* default initialize wts to unity */
+		fits->weights [ ichan ]      = 1.0;
+		fits->scales[ ichan ]        = 1.0;
+		fits->offsets[ ichan ]       = 0.0;
+	}
+
+	fits->tsamp_tbin = period / nbin;
+	fits->mjd        = mjd;
 }
 
-void gmrtfits_create ( gmrtfits_t *fits ) {
+void gmrtfits_open ( gmrtfits_t *fits ) {
 	fits->status = 0;
 
 	int bitpix   = 8;
 	int naxis    = 0;
 	long *naxes  = NULL;
 	int ival     = 0;
+	char   cval  = 0;
 	double dval  = 0.0;
 	float  fval  = 0.0;
 	long   lval  = 0;
@@ -75,6 +121,8 @@ void gmrtfits_create ( gmrtfits_t *fits ) {
 	fits_create_file(&fits->fits, fits->filename, &fits->status);
 	fits_create_img(fits->fits, bitpix, naxis, naxes, &fits->status);
 
+	ival = 1;
+	fits_update_key(fits->fits, TSTRING, "HDRVER","5.4gmrt","Header version, modified for uGMRT", &fits->status);
 	fits_update_key(fits->fits, TSTRING, "HDRVER","5.4gmrt","Header version, modified for uGMRT", &fits->status);
 	fits_update_key(fits->fits,TSTRING,"FITSTYPE","GMRTFITS","PSRFITS definition for uGMRT beamform data",&fits->status);
 
@@ -103,7 +151,12 @@ void gmrtfits_create ( gmrtfits_t *fits ) {
 	fits_update_key(fits->fits, TFLOAT,"FD_SANG",&fval,"[deg] FA of E vect for equal sig in A&B (E) ", &fits->status);
 
 	// obsmode
-	fits_update_key (fits->fits, TSTRING, "OBS_MODE", "SEARCH", "SEARCH by default", &fits->status );
+	if ( fits->obsmode == SEARCH )
+		fits_update_key (fits->fits, TSTRING, "OBS_MODE", "SEARCH", "SEARCH, PSR or CAL", &fits->status );
+	else if ( fits->obsmode == FOLD )
+		fits_update_key (fits->fits, TSTRING, "OBS_MODE", "PSR", "SEARCH, PSR or CAL", &fits->status );
+	else
+		fprintf ( stderr, " fits->obsmode not understood \n" );
 
 	// frequency
 	dval = fits->center_freqs [ fits->nchan / 2 ];
@@ -112,7 +165,7 @@ void gmrtfits_create ( gmrtfits_t *fits ) {
 	fits_update_key (fits->fits, TDOUBLE, "OBSBW", &dval, "[MHz] Bandwidth of observation", &fits->status);
 	ival = fits->nchan;
 	fits_update_key (fits->fits, TINT, "NCHAN", &ival, "Number of channels", &fits->status);
-	ival = 1;
+	ival = fits->nbin;
 	fits_update_key(fits->fits, TINT, "NBIN", &ival, "Nr of bins (PSR/CAL mode; else 1) ", &fits->status);
 
 	// coordinates
@@ -151,15 +204,15 @@ void gmrtfits_create ( gmrtfits_t *fits ) {
 	}
 }
 
-void gmrtfits_subint_open ( gmrtfits_t *fits ) {
+void gmrtfits_data_table ( gmrtfits_t *fits ) {
 	/*char *ttypes, *tforms, *tunits;*/
 
-	char *ttypes[] = { "ISUBINT", "TSUBINT", "OFFS_SUB", "NPOL", "DAT_FREQ", "DAT_WTS", "DAT_OFFS", "DAT_SCL", "DATA"};
-	char *tunits[] = {"", "s", "s", "", "MHz", "", "", "", ""};
-	/*char tforms[9][16];*/
-	char *tforms[9];
+	/* adding period column in both modes */
+	char *ttypes[] = { "ISUBINT", "TSUBINT", "OFFS_SUB", "NPOL", "PERIOD", "DAT_FREQ", "DAT_WTS", "DAT_OFFS", "DAT_SCL", "DATA"};
+	char *tunits[] = {"", "s", "s", "", "s", "MHz", "", "", "", ""};
+	char *tforms[10];
 	int i;
-	for (i = 0; i < 9; i++) tforms[i] = malloc ( sizeof(char) * 16 );
+	for (i = 0; i < 10; i++) tforms[i] = malloc ( sizeof(char) * 16 );
 	long naxes [3];
 
 	int ival;
@@ -169,22 +222,35 @@ void gmrtfits_subint_open ( gmrtfits_t *fits ) {
 	sprintf (tforms[1], "1D");
 	sprintf (tforms[2], "1D");
 	sprintf (tforms[3], "1J");
-	sprintf (tforms[4], "%dD", fits->nchan );
-	sprintf (tforms[5], "%dD", fits->nchan_npol );
-	sprintf (tforms[6], "%dD", fits->nchan_npol );
-	sprintf (tforms[7], "%dD", fits->nchan_npol );
-	sprintf (tforms[8], "%dB", fits->nsblk_nchan_npol );
+	sprintf (tforms[4], "1D");
+	sprintf (tforms[5], "%dE", fits->nchan );
+	sprintf (tforms[6], "%dE", fits->nchan_npol );
+	sprintf (tforms[7], "%dE", fits->nchan_npol );
+	sprintf (tforms[8], "%dE", fits->nchan_npol );
+	if ( fits->obsmode == SEARCH ) sprintf (tforms[9], "%dB", fits->nsblk_nchan_npol );
+	else if ( fits->obsmode == FOLD ) sprintf (tforms[9], "%dI", fits->nbin_nchan_npol );
+	/*else if ( fits->obsmode == FOLD ) sprintf (tforms[9], "%dE", fits->nbin_nchan_npol );*/
 
-	fits_create_tbl( fits->fits, BINARY_TBL, 0, 9, ttypes, tforms, tunits, "SUBINT", &fits->status);
+	fits_create_tbl( fits->fits, BINARY_TBL, 0, 10, ttypes, tforms, tunits, "SUBINT", &fits->status);
 	// free them
-	for (i = 0; i < 9; i++) free ( tforms[i] );
+	for (i = 0; i < 10; i++) free ( tforms[i] );
 
-	// astropy.io.fits needs me to save in reverse order
-	naxes[0] = fits->nsblk;
-	naxes[1] = fits->npol;
-	naxes[2] = fits->nchan;
+	// cfitsio fits needs me to save in reverse order
+	if ( fits->obsmode == SEARCH ) {
+		/* documentation = (nchan, npol, nsblk) */
+		/* data          = (nsblk, npol, nchan) */
+		naxes[0] = fits->nsblk;
+		naxes[1] = fits->npol;
+		naxes[2] = fits->nchan;
+	} else if ( fits->obsmode == FOLD ) {
+		/* documentation = (nbin, nchan, npol) */
+		/* data          = (nbin, nchan, npol) */
+		naxes[0] = fits->nbin;
+		naxes[1] = fits->nchan;
+		naxes[2] = fits->npol;
+	}
 
-	fits_write_tdim( fits->fits, 9, 3, naxes, &fits->status );
+	fits_write_tdim( fits->fits, 10, 3, naxes, &fits->status );
 
 	fits->nrow     = 1;
 
@@ -202,151 +268,38 @@ void gmrtfits_subint_open ( gmrtfits_t *fits ) {
 	fits_update_key(fits->fits,TINT,"NSBLK",&ival,"Number of samples per block.",&fits->status);
 	ival  = fits->bitdepth;
 	fits_update_key(fits->fits,TINT,"NBITS",&ival,"Number of bits per datum.",&fits->status);
-	ival  = 1;
+	ival  = fits->nbin;
 	fits_update_key(fits->fits, TINT, "NBIN", &ival, "Nr of bins (PSR/CAL mode; else 1) ", &fits->status);
-	fval  = fits->tsamp;
+	fval  = fits->tsamp_tbin;
 	fits_update_key(fits->fits, TFLOAT, "TBIN", &fval, "[s] Time per bin or sample ", &fits->status);
+	fval  = fits->dm;
+	fits_update_key(fits->fits, TFLOAT, "DM", &fval, "[pc/cc] dispersion measure", &fits->status);
 
 	fits_update_key(fits->fits,TSTRING,"POL_TYPE",fits->pol_type,"Polarization type.",&fits->status);
 
 	//
 	fits_flush_file (fits->fits, &fits->status);
-
 }
 
-void gmrtfits_subint_real ( gmrtfits_t *fits, real_t *data, unsigned int start, unsigned int ngulp ) {
-	// the data layout is confusing since CFITSIO behaves like FORTRAN with column major ordering
-	// need to write (start:(start+nsblk), npol, nchan)
+void gmrtfits_close ( gmrtfits_t *fits ) {
+	free ( fits->center_freqs );
+	free ( fits->offsets );
+	free ( fits->scales );
+	free ( fits->weights );
+
+	if (fits->obsmode == SEARCH) free ( fits->data );
+	else if (fits->obsmode == FOLD) free ( fits->bata );
+
+	free ( fits->filename );
 	
-	int colnum  = 0;
-	int    ival = 0;
-	double dval = 0.0;
 
-	// normalize data
-	// scales and offsets are (nchan, npol)
-	// DATA = DAT_WTS * ( BATA*SCALES + OFFSETS )
-	
-	// iterators
-	unsigned isamp, osamp, ichan, ipol;
-	unsigned stop  = start + fits->nsblk;
-
-	// compute min and max (nchan, npol)
-	// min is offset and max-min is scales
-	real_t _offset, _scale;
-
-#ifdef TIMING
-	clock_t tstart, tstop;
-	double time_process, time_fitsio, time_flush;
-	tstart  = clock ();
-#endif
-
-	// data is (nsblk, npol, nchan)
-	// i am sure of this
-	for (ipol = 0; ipol < fits->npol; ipol++) {
-		for (ichan = 0; ichan < fits->nchan; ichan++) {
-
-			real_t _min = FLT_MAX, _max = -FLT_MAX;
-
-			// find min and max per (nchan,npol)
-			for (isamp  = start; isamp < stop; isamp++) {
-				real_t _d = data [ ichan + fits->nchan*ipol + fits->nchan_npol*isamp ];
-
-				if ( _d < _min ) _min = _d;
-				if ( _d > _max ) _max = _d;
-			} // nsamp
-				
-			_offset    = _min;
-			_scale     = (_max - _min) / 255.0;
-
-			fits->offsets [ ichan + fits->nchan*ipol ] = _offset;
-			fits->scales  [ ichan + fits->nchan*ipol ] = _scale;
-
-			for (isamp = start,osamp = 0; isamp < stop; isamp++, osamp++) {
-				real_t _d = data [ ichan + fits->nchan*ipol + fits->nchan_npol*isamp ];
-				fits->data [ ichan + fits->nchan*ipol + fits->nchan_npol*osamp ] = (char) ( ( _d - _offset ) / _scale );
-				/*fits->data [ ichan + fits->nchan*ipol + fits->nchan_npol*osamp ] = (char) ( ipol + (64*ichan/2048));*/
-			} // nsamp
-		} // freq
-	} // pol
-		
-#ifdef TIMING
-	tstop        = clock ();
-	time_process = (tstop - tstart) / CLOCKS_PER_SEC;
-	tstart       = clock ();
-#endif
-	
-	// ISUBINT
-	colnum  = 1;
-	fits_write_col( fits->fits, TINT, colnum, fits->nrow, 1, 1, &fits->nrow, &fits->status );
-
-	// TSUBINT
-	colnum  = 2;
-	dval    = fits->nsblk * fits->tsamp;
-	fits_write_col( fits->fits, TDOUBLE, colnum, fits->nrow, 1, 1, &dval, &fits->status );
-	
-	// OFFS_SUB
-	colnum  = 3;
-	dval    =  (fits->nrow + 0.5) * fits->nsblk * fits->tsamp;
-	fits_write_col( fits->fits, TDOUBLE, colnum, fits->nrow, 1, 1, &dval, &fits->status );
-	// middle of the subint
-	
-	// NPOL
-	colnum  = 4;
-	ival    = fits->npol;
-	fits_write_col( fits->fits, TINT, colnum, fits->nrow, 1, 1, &ival, &fits->status );
-
-	// DAT_FREQ
-	colnum  = 5;
-	fits_write_col( fits->fits, TFLOAT, colnum, fits->nrow, 1, fits->nchan, fits->center_freqs, &fits->status );
-
-	// DAT_WTS
-	colnum  = 6;
-	fits_write_col( fits->fits, TFLOAT, colnum, fits->nrow, 1, fits->nchan_npol, fits->weights, &fits->status );
-
-	// DAT_OFFS
-	colnum  = 7;
-	fits_write_col( fits->fits, TFLOAT, colnum, fits->nrow, 1, fits->nchan_npol, fits->offsets, &fits->status );
-
-	// DAT_SCL
-	colnum  = 8;
-	fits_write_col( fits->fits, TFLOAT, colnum, fits->nrow, 1, fits->nchan_npol, fits->scales, &fits->status );
-
-	// DATA
-	colnum  = 9;
-	fits_write_col( fits->fits, TBYTE, colnum, fits->nrow, 1, fits->nsblk_nchan_npol, fits->data, &fits->status );
-
-
+	fits_close_file ( fits->fits, &fits->status );
 	if ( fits->status ) {
-		printf ("\n at gmrtfits_subint_real_single row=%ld ...\n", fits->nrow);
 		fits_report_error(stderr, fits->status);
-		raise(SIGINT);
-		/*exit (1);*/
 	}
-
-#ifdef TIMING
-	tstop        = clock ();
-	time_fitsio  = (tstop - tstart) / CLOCKS_PER_SEC;
-#endif
-
-	if ( fits->nrow % FLUSH_TIMES == 0) {
-#ifdef TIMING
-		tstart       = clock ();
-#endif
-		fits_flush_buffer( fits->fits, 0, &fits->status );
-#ifdef TIMING
-		tstop        = clock ();
-		time_flush   = (tstop - tstart) / CLOCKS_PER_SEC;
-#endif
-	}
-
-#ifdef TIMING
-	printf ("[TIMING] process=%.5f fitsio=%.5f flush=%.5f\n", time_process, time_fitsio, time_flush);
-#endif
-
-	fits->nrow++;
 }
 
-void gmrtfits_subint_add ( gmrtfits_t *fits, real_t *data, unsigned int ngulp ) {
+void gmrtfits_search_add ( gmrtfits_t *fits, real_t *data, unsigned int ngulp ) {
 	// the data layout is confusing since CFITSIO behaves like FORTRAN with column major ordering
 	// ngulp = k * nsblk for some k
 	// will write multiple times in the same function call
@@ -420,12 +373,12 @@ void gmrtfits_subint_add ( gmrtfits_t *fits, real_t *data, unsigned int ngulp ) 
 
 		// TSUBINT
 		colnum  = 2;
-		dval    = fits->nsblk * fits->tsamp;
+		dval    = fits->nsblk * fits->tsamp_tbin;
 		fits_write_col( fits->fits, TDOUBLE, colnum, fits->nrow, 1, 1, &dval, &fits->status );
 		
 		// OFFS_SUB
 		colnum  = 3;
-		dval    =  (fits->nrow + 0.5) * fits->nsblk * fits->tsamp;
+		dval    =  (fits->nrow + 0.5) * fits->nsblk * fits->tsamp_tbin;
 		fits_write_col( fits->fits, TDOUBLE, colnum, fits->nrow, 1, 1, &dval, &fits->status );
 		// middle of the subint
 		
@@ -482,6 +435,166 @@ void gmrtfits_subint_add ( gmrtfits_t *fits, real_t *data, unsigned int ngulp ) 
 #ifdef TIMING
 	printf ("[TIMING] written rows=%d process=%.5f fitsio=%.5f flush=%.5f\n", rowwrites, time_process, time_fitsio, time_flush);
 #endif
+} // search_add
 
-} // function
+void gmrtfits_fold_add ( gmrtfits_t *fits, deal_t *data, unsigned nturns, double period ) {
+	/* data is (nbin, nchan, npol)  */
+	
+	int colnum  = 0;
+	int    ival = 0;
+	double dval = 0.0;
 
+	// normalize data
+	// scales and offsets are (nchan, npol)
+	// DATA = DAT_WTS * ( BATA*SCALES + OFFSETS )
+	
+	// iterators
+	unsigned ibin, osamp, ichan, ipol;
+	unsigned int start, stop;
+
+	// compute min and max (nchan, npol)
+	// min is offset and max-min is scales
+	real_t _offset, _scale;
+
+#ifdef TIMING
+	clock_t tstart, tstop;
+	unsigned rowwrites  = 0;
+	double time_process = 0., time_fitsio = 0., time_flush = 0.;
+#endif
+
+#ifdef TIMING
+		tstart  = clock ();
+#endif
+	// data is (nbin, nchan, npol)
+	for (ichan = 0; ichan < fits->nchan; ichan++) {
+		for (ipol = 0; ipol < fits->npol; ipol++) {
+			deal_t _min = FLT_MAX, _max = -FLT_MAX;
+
+			// find min and max per (nchan,npol)
+			for (ibin = 0; ibin < fits->nbin; ibin++) {
+				deal_t _d = data [ ipol + fits->npol*ichan + fits->nchan_npol*ibin ];
+
+				if ( _d < _min ) _min = _d;
+				if ( _d > _max ) _max = _d;
+			} // bin
+
+			/**
+			 * min -> INT16_MIN
+			 * max -> INT16_MAX
+			 *
+			 * (b - bmin) / (d - min) = ( INT16_MAX - INT16_MIN ) / ( max - min )
+			 * (b - bmin) = ( INT16_MAX - INT16_MIN ) / ( max - min ) * ( d - min )
+			 * b  = (INT16_MAX-INT16_MIN)/(max-min) * d + ( bmin - min*(INT16_MAX-INT16_MIN)/(max-min) )
+			 * ss = (INT16_MAX-INT16_MIN)/(max-min) 
+			 * b  = ss * d + ( INT16_MIN - min*ss ) 
+			 *
+			 * bmin -> dmin
+			 * bmax -> dmax
+			 *
+			 * slope = 
+			 *
+			 * data = scl*bata + offs
+			 *
+			 * scl  = (dmax - dmin) / (INT16_MAX - INT16_MIN)
+			 *
+			 * (d - dmin) / ( b - bmin ) = scl
+			 * d - dmin = scl*b - scl*bmin
+			 * d = scl*b + dmin - scl*bmin
+			 *
+			 *
+			 **/
+					
+			_scale     = (_max - _min) / (INT16_MAX - INT16_MIN);
+			_offset    = _min - _scale*INT16_MIN;
+
+			fits->offsets [ ichan + fits->nchan*ipol ] = _offset;
+			fits->scales  [ ichan + fits->nchan*ipol ] = _scale;
+
+			for (ibin = 0; ibin < fits->nbin; ibin++) {
+				size_t idx = ipol + fits->npol*ichan + fits->nchan_npol*ibin;
+				size_t jdx = ibin + fits->nbin*ichan + fits->nbin*fits->nchan*ipol;
+				deal_t _d  = data [ idx ];
+				fits->bata [ jdx ] = (int16_t) ( ( _d - _offset ) / _scale );
+			} // bin
+		} // pol
+	} // chan
+#ifdef TIMING
+		tstop        = clock ();
+		time_process += (tstop - tstart) / CLOCKS_PER_SEC;
+		tstart       = clock ();
+#endif
+
+		// ISUBINT
+		colnum  = 1;
+		fits_write_col( fits->fits, TINT, colnum, fits->nrow, 1, 1, &fits->nrow, &fits->status );
+
+		// TSUBINT
+		colnum  = 2;
+		dval    = nturns * fits->period;
+		fits_write_col( fits->fits, TDOUBLE, colnum, fits->nrow, 1, 1, &dval, &fits->status );
+		
+		// OFFS_SUB
+		colnum  = 3;
+		dval    =  (fits->nrow + 0.5) * nturns * fits->period;
+		fits_write_col( fits->fits, TDOUBLE, colnum, fits->nrow, 1, 1, &dval, &fits->status );
+		// middle of the subint
+		
+		// NPOL
+		colnum  = 4;
+		ival    = fits->npol;
+		fits_write_col( fits->fits, TINT, colnum, fits->nrow, 1, 1, &ival, &fits->status );
+
+		// PERIOD
+		colnum  = 5;
+		dval    =  period;
+		fits_write_col( fits->fits, TDOUBLE, colnum, fits->nrow, 1, 1, &dval, &fits->status );
+
+		// DAT_FREQ
+		colnum  = 6;
+		fits_write_col( fits->fits, TFLOAT, colnum, fits->nrow, 1, fits->nchan, fits->center_freqs, &fits->status );
+
+		// DAT_WTS
+		colnum  = 7;
+		fits_write_col( fits->fits, TFLOAT, colnum, fits->nrow, 1, fits->nchan_npol, fits->weights, &fits->status );
+
+		// DAT_OFFS
+		colnum  = 8;
+		fits_write_col( fits->fits, TFLOAT, colnum, fits->nrow, 1, fits->nchan_npol, fits->offsets, &fits->status );
+
+		// DAT_SCL
+		colnum  = 9;
+		fits_write_col( fits->fits, TFLOAT, colnum, fits->nrow, 1, fits->nchan_npol, fits->scales, &fits->status );
+
+		// DATA
+		colnum  = 10;
+		fits_write_col( fits->fits, TSHORT, colnum, fits->nrow, 1, fits->nbin_nchan_npol, fits->bata, &fits->status );
+		/*fits_write_col( fits->fits, TFLOAT, colnum, fits->nrow, 1, fits->nbin_nchan_npol, fits->bata, &fits->status );*/
+
+
+		if ( fits->status ) {
+			printf ("\n at gmrtfits_subint_real_single row=%ld ...\n", fits->nrow);
+			fits_report_error(stderr, fits->status);
+			raise(SIGINT);
+			/*exit (1);*/
+		}
+
+#ifdef TIMING
+		tstop        = clock ();
+		time_fitsio  += (tstop - tstart) / CLOCKS_PER_SEC;
+		tstart       = clock ();
+#endif
+		if ( fits->nrow % FLUSH_TIMES == 0 ) {
+			fits_flush_buffer( fits->fits, 0, &fits->status );
+		}
+#ifdef TIMING
+		tstop        = clock ();
+		time_flush   += (tstop - tstart) / CLOCKS_PER_SEC;
+		rowwrites++;
+#endif
+
+		fits->nrow++;
+
+#ifdef TIMING
+	printf ("[TIMING] written rows=%d process=%.5f fitsio=%.5f flush=%.5f\n", rowwrites, time_process, time_fitsio, time_flush);
+#endif
+} // fold_add
--- gmrtfits.h
+++ gmrtfits.h
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <stddef.h>
 #include <float.h>
 #include <math.h>
@@ -14,6 +15,10 @@
 /*typedef uint16_t u16_t;*/
 /*typedef uint8_t u8_t;*/
 typedef float real_t;
+typedef float deal_t;
+/*typedef double deal_t;*/
+
+typedef enum { SEARCH, FOLD } fitsmode_t;
 
 
 typedef struct {
@@ -21,7 +26,10 @@ typedef struct {
 	int status;
 
 	double mjd;
-	double tsamp;
+	/* tsamp or tbin */
+	double tsamp_tbin;
+	double period;
+	float dm;
 
 	// RA and DEC in degrees
 	float rad, decd;
@@ -33,6 +41,9 @@ typedef struct {
 	unsigned nchan, nsblk, npol;
 	unsigned nchan_npol;
 	unsigned nsblk_nchan_npol;
+
+	unsigned nbin;
+	unsigned nbin_nchan_npol;
 	unsigned bitdepth;
 
 	float *center_freqs;
@@ -48,30 +59,28 @@ typedef struct {
 	// to manage IO
 	float *weights, *scales, *offsets;
 	char  *data;
+	int16_t *bata;
+	/*float *bata;*/
 
+	// mode
+	fitsmode_t obsmode;
 } gmrtfits_t;
 
 // XXX make it like FFTW
 // returns pointer to gmrtfits_t
-void gmrtfits_prepare ( gmrtfits_t *fits, const char *filename, double mjd, unsigned npol, unsigned nchan, float fedge, float bw, unsigned nsblk, unsigned fftint, unsigned bitdepth );
+void gmrtfits_search_prepare ( gmrtfits_t *fits, const char *filename, double mjd, unsigned npol, unsigned nchan, float fedge, float bw, unsigned nsblk, unsigned fftint, unsigned bitdepth );
+void gmrtfits_fold_prepare ( gmrtfits_t *fits, const char *filename, double mjd, unsigned npol, unsigned nchan, float fedge, float bw, unsigned nbin, double period );
 
-// public methods
-void gmrtfits_create ( gmrtfits_t *fits );
+/* IO methods */
+void gmrtfits_open ( gmrtfits_t *fits );
+void gmrtfits_data_table ( gmrtfits_t *fits );
 void gmrtfits_close ( gmrtfits_t *fits );
 
-void gmrtfits_subint_open ( gmrtfits_t *fits );
-
-/*void add_subint_16bit ( gmrtfits_t *fits, u16_t *data );*/
-void gmrtfits_subint_real ( gmrtfits_t *fits, real_t *data, unsigned int start, unsigned int ngulp );
-// XXX maybe in future, it would be worthwhile to write another
-// gmrtfits_subint function that directly takes in u8 data
-/*void add_subint_8bit ( gmrtfits_t *fits, u8_t *data );*/
-// it would skip the normalize_data step
-
-// same as above
-// but writes all the data in one go
-// avoiding a loop somewhere out
-void gmrtfits_subint_add ( gmrtfits_t *fits, real_t *data, unsigned int ngulp );
+/* *
+ *
+ * */
+void gmrtfits_search_add ( gmrtfits_t *fits, real_t *data, unsigned int ngulp );
+void gmrtfits_fold_add ( gmrtfits_t *fits, deal_t *data, unsigned nturns, double period );
 
 // private methods
 
